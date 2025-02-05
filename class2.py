d615cd206ecb3df7486fb687a0f71d3877ebff11
Ashutoshh Singh <asashuthegreat4@gmail.com>
2025-02-05 14:23:11 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,12 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+number_1 = 100 
+my_name = "ashutoshh"
+
+x = 300 
+
+# below are some examples of print statement 
+print(x)
+print(my_name)
+print(x,number_1)
+print(x,my_name)
---
+++ operator.py
@@ -0,0 +1,33 @@
+# taking input from user 
+z = 100 
+x = 200
+w = 200
+
+sum = x + z
+subs1 = z - x
+print(sum)
+print(subs1)
+
+print(200/15)
+print(200*15)
+
+# using comparasion operator 
+
+print(x > z)
+print(x < z)
+print( w >= x)
+
+## using some more 
+#  = (assignment operator)
+x1 = "hello world"
+y1 ="hii"
+
+#x1 = y1 
+print(x1)
+#  comparasion operator  == 
+print( 5 == 5)
+print (x1 == y1 )
+
+## 
+print(3**3)
+
