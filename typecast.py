4ee61ad3200a9fdc56b79b75195d2d7e443cbbf2
Ashutoshh Singh <asashuthegreat4@gmail.com>
2025-02-13 13:40:36 +0530

    Create typecast.py

---
+++ typecast.py
@@ -0,0 +1,10 @@
+
+print("Enter any number : ")
+n1=input()
+# print the type of n1 and n2 
+print(type(n1))
+# by default input function will accept everythin as string
+# now we need to convert n1 and n2 from STR to INT
+print("lets type cast to INT")
+n1_new = int(n1)
+print(type(n1_new))
