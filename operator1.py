2f970f5038d1b02f8a28d9bc1ddb5fc504e251c8
Ashutoshh Singh <asashuthegreat4@gmail.com>
2025-02-13 14:01:02 +0530

    Create operator1.py

---
+++ operator1.py
@@ -0,0 +1,27 @@
+n1=input("enter first number :  ")
+n2=input("enter second number :  ")
+# lets type cast them first
+print("type casting both numbers ")
+n1_new = int(n1)
+n2_new = int(n2)
+
+# using + operator 
+sum = n1_new + n2_new 
+print("sum of above given inputs are ",sum)
+
+# using - operator 
+diff1 = n1_new - n2_new 
+print("difference of above given inputs are ",diff1)
+
+# using * operator 
+multi = n1_new * n2_new 
+print("multiplication of above given inputs are ",multi)
+
+# using / operator 
+div1 = n1_new / n2_new 
+print("division of above given inputs are ",div1)
+# using comparasion operator 
+print("using comparasion operation n1_new > n2_new")
+print(n1_new > n2_new)
+print("using comparasion operation n1_new == n2_new")
+print(n1_new == n2_new)
